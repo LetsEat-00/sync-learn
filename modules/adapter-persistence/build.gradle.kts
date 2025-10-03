71c28e6c76c5c570849b17d4b95e6b1097f9ca21
limcpf <daeseong0226@gmail.com>
2025-10-03 20:18:16 +0900

    fix: CreatedDate 작동을 위해 AuditingEntityListener 추가 및 flyway 경고문 삭제를 위한 postgresql 의존성 및 flyway 의존성 추가

--- modules/adapter-persistence/build.gradle.kts
+++ modules/adapter-persistence/build.gradle.kts
@@ -1,10 +1,13 @@
 dependencies {
     implementation(project(":modules:domain"))
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.flywaydb:flyway-core")
+    implementation("org.flywaydb:flyway-database-postgresql") // ★ 추가
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.testcontainers:junit-jupiter")
     testImplementation("org.testcontainers:postgresql")
-    testImplementation("org.flywaydb:flyway-core")
     testImplementation("org.springframework.boot:spring-boot-testcontainers")
+
+    testRuntimeOnly("org.postgresql:postgresql")
 }
